1bf2945c9d6d1023862436cb96d562b7cef4714e
Sabrinachen1205 <138372818+Sabrinachen1205@users.noreply.github.com>
2024-04-14 15:42:05 +0800

    Add files via upload

---
+++ leetcode (189. Rotate Array).py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        l = len(nums)
+        k = k % l
+        nums[:] = nums[l-k:] + nums[:l-k]
+        return
