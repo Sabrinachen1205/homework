860a7761bb08fc24677b81a6590a4e4b3b4fc003
Sabrinachen1205 <138372818+Sabrinachen1205@users.noreply.github.com>
2024-04-14 14:42:05 +0800

    Add files via upload

---
+++ leetcode (57. Insert Interval).py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def insert(self, intervals, newInterval):
+        """
+        :type intervals: List[List[int]]
+        :type newInterval: List[int]
+        :rtype: List[List[int]]
+        """
+        n = len(intervals)
+        ans = []
+        i = 0
+
+        while i < n and intervals[i][1] < newInterval[0]:
+            ans.append(intervals[i])
+            i += 1
+
+        while i < n and intervals[i][0] <= newInterval[1]:
+            newInterval[0] = min(newInterval[0], intervals[i][0])
+            newInterval[1] = max(newInterval[1], intervals[i][1])
+            i += 1
+        ans.append(newInterval)
+
+        while i < n:
+            ans.append(intervals[i])
+            i += 1
+
+        return ans
