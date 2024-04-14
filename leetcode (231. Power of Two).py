b829d972b90861c74911036508cc7a49ad11f49a
Sabrinachen1205 <138372818+Sabrinachen1205@users.noreply.github.com>
2024-04-14 16:02:45 +0800

    Add files via upload

---
+++ leetcode (231. Power of Two).py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n == 1:
+            return True
+        elif n==0:
+            return False        
+        elif n % 2 != 0:
+            return False
+        else:
+            return self.isPowerOfTwo(n/2)
+        
