654dfd0c8e60b7e44d1a0269d0f9fa965d9d80da
Sabrinachen1205 <138372818+Sabrinachen1205@users.noreply.github.com>
2024-04-16 13:32:55 +0800

    Add files via upload

---
+++ leetcode (796. Rotate String).py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def rotateString(self, s, goal):
+        """
+        :type s: str
+        :type goal: str
+        :rtype: bool
+        """
+        if len(s) != len(goal):
+            return False
+        return goal in (s + s)
